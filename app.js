90a4fe1f4ceefe3e6688286a00b337b4601f8a23
DVermeer <duncan.vermeer@gmail.com>
2021-05-24 21:54:49 +0200

    update counters

--- app.js
+++ app.js
@@ -8,10 +8,9 @@ function createTodo(title) {
   // create a checkbox
   const cb = document.createElement("input");
   cb.type = "checkbox";
+  cb.onclick = "updateCounters()";
 
   // add the "change" event listener to the checkbox
-  // and append the checkbox to the label
-  label.appendChild(cb);
 
   // create a text node with the given title
   var labelText = document.createTextNode(title);
@@ -20,7 +19,8 @@ function createTodo(title) {
 
   // create a list item
   const listItem = document.createElement("li");
-  // and append the label to list item
+  // and append the checkbox and label to list item
+  listItem.appendChild(cb);
   listItem.appendChild(labelText);
   // append the list item to the todo list
   const list = document.getElementById("todoList");
@@ -35,19 +35,25 @@ document
     var data = document.getElementById("newToDo").value;
     console.log(data);
     createTodo(data);
-    document.getElementById("newToDo").value = "new todo";
+    document.getElementById("newToDo").value = "";
+    updateCounters();
   });
 
 function updateCounters() {
   // declare a variable that contains the "Total" counter element
   var totalCount = document.querySelectorAll("li");
-  console.log("Total Amount: " + totalCount);
+  console.log("Total Amount: " + totalCount.length);
+  document.getElementById("totalCounter").innerText = totalCount.length;
   // declare a variable that contains the number of todos, by counting how many elements have a specific classname / attribute
-  var countDone = document.querySelector(".itemsDone");
+  var countDone = document.querySelectorAll(
+    'input[type="checkbox"]:checked'
+  ).length;
   console.log("Done: " + countDone);
-  var countToDo = document.querySelector(".itemsToDo");
+  var countToDo = totalCount.length - countDone;
   console.log("To Do: " + countToDo);
   // update the HTML inside the counter element with the number of todos
   document.querySelector(".totalCounter");
+  document.getElementById("todoCounter").innerText = countToDo;
+  document.getElementById("doneCounter").innerText = countDone;
 }
 updateCounters();
